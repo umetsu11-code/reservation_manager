0f7b844473315c32db780710ed3e00f742b91e4e
umetsu sunao <s.umetsu11@gmail.com>
2024-11-29 20:48:13 +0900

    deviseにてログアウト機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_23_033610) do
+ActiveRecord::Schema.define(version: 2024_11_29_112351) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "remember_created_at"
   end
 
 end
