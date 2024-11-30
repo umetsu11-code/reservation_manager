16ac4705e5f9950e7827eeecb4e8f90ff7688609
umetsu sunao <s.umetsu11@gmail.com>
2024-11-30 17:23:31 +0900

    deviseのviewディレクトリを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
 
   # ログイン後のリダイレクト先をトップページ（root_path）に設定
   def after_sign_in_path_for(resource)
-    root_path
+    root_path #ログイン画面に遷移
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path # ホーム画面（トップページ）にリダイレクト
+    new_user_session_path #ログイン画面に遷移
   end
 
 end
